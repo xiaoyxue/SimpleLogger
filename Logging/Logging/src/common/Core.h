8a5ddd41540d18ff90ea75f6285c0de9e95aedcf
Xiaoyu Xue <caorenxxy@gmail.com>
2020-12-05 21:39:20 +0800

    change

--- Logging/Logging/src/common/Core.h
+++ Logging/Logging/src/common/Core.h
@@ -8,14 +8,16 @@
 
 GYT_NAMESPACE_BEGIN
 
-#define SPD_LOG(X, ...) Gyunity::GetLogger().X(fmt::format(__VA_ARGS__))
+#define SPD_LOG(X, ...) Gyunity::GetDefaultLogger()->X(fmt::format(__VA_ARGS__))
+
+#define SPD_INFO_LOG(X, ...) Gyunity::GetInfoLogger()->X(fmt::format(__VA_ARGS__))
 
 #define SPD_LOCATED_LOG(X, ...)                                             \
-  Gyunity::GetLogger().X(                                                   \
-      fmt::format("[{}:{}@{}] ", __FILE__, __FUNCTION__, __LINE__) +        \
+  Gyunity::GetDefaultLogger()->X(                                           \
+      fmt::format("[{}: {} @{}] ", __FILE__, __FUNCTION__, __LINE__) +        \
       fmt::format(__VA_ARGS__))
 
-#define GYT_SET_LOG_LEVEL(Level) Gyunity::GetLogger().SetLevel(Level)
+#define GYT_SET_LOG_LEVEL(Level) Gyunity::GetDefaultLogger()->SetLevel(Level)
 #define GYT_INFO(...) SPD_LOG(Info, __VA_ARGS__)
 #define GYT_WARN(...) SPD_LOCATED_LOG(Warn, __VA_ARGS__)
 #define GYT_DEBUG(...) SPD_LOCATED_LOG(Debug, __VA_ARGS__)
@@ -27,5 +29,6 @@ do                                              \
     GYT_UNREACHABLE                             \
 } while (0)
 
+#define GYT_PRINT(...) SPD_INFO_LOG(Info, __VA_ARGS__)
 
 GYT_NAMESPACE_END
--- Logging/Logging/src/main.cpp
+++ Logging/Logging/src/main.cpp
@@ -3,6 +3,14 @@
 
 using namespace Gyunity;
 
+class A {
+public:
+	std::string a = "abc";
+	std::string ToString() {
+		return fmt::format("a: {}", a);
+	}
+};
+
 int main()
 {
 	//Logger logger;
@@ -11,11 +19,15 @@ int main()
 	//logger.Warn("Warn");
 	//logger.Debug("Debug");
 	//logger.Error("Error");
+
 	GYT_SET_LOG_LEVEL("Debug");
 	GYT_INFO("Info");
 	GYT_INFO("Test {} {} {}", 1, 2, 3);
 	GYT_WARN("Warn");
 	GYT_DEBUG("Debug");
-	GYT_ERROR("Error");
+	GYT_PRINT("sdfdsfsdf");
+	A a;
+	GYT_PRINT("\n{}\n", a.ToString());
+	//GYT_ERROR("Error");
 	return 0;
 }
